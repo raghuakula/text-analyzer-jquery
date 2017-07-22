6b6f72ac84369e804b966a27af1af740d2a07ee8
Raghu Akula <akula.raghu@gmail.com>
2017-07-22 09:32:35 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// your code here!
+
+
+//display values for Word count, Unique word count, and Average word length
+
+/*
+function handleAnalysis {
+
+   //get button input 
+   $(.button).submit
+
+
+
+}
+
+
+
+
+$(handleAnalysis)
+*/
